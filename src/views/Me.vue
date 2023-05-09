8c62ff9e4ef787263a56e9a9a498909408ba9ae7
rey <3065588496@qq.com>
2023-05-09 23:29:28 +0800

    about me

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,63 +1,194 @@
 <template>
   <div class="me">
-    <div class="top">
-      <img class="top-cloud" src="../assets/me/imgs/cloud.png" alt="" />
-      <h1>Yongteng Lei</h1>
-      <h2>A <span>pro</span>grammer</h2>
-      <img class="bottom-cloud" src="../assets/me/imgs/cloud.png" alt="" />
-      <img src="../assets/me/imgs/mountain.png" alt="" />
-    </div>
+    <div>
+      <div class="top">
+        <img class="top-cloud" src="../assets/me/imgs/cloud.png" alt="" />
+        <h1>Yongteng Lei</h1>
+        <h2>A <span>pro</span>grammer</h2>
+        <img class="bottom-cloud" src="../assets/me/imgs/cloud.png" alt="" />
+        <img src="../assets/me/imgs/mountain.png" alt="" />
+      </div>
 
-    <div class="middle-container">
-      <div class="profile">
-        <img src="../assets/me/imgs/200.webp" alt="" />
-        <h2>Hello.</h2>
-        <p class="intro">
-          Lorem ipsum dolor sit amet, vitae volutpat, dui conubia, dolor urna.
-          Non auctor, montes nulla distinctio.
-        </p>
-      </div>
-      <hr />
-      <div class="skills">
+      <div class="container-fluid">
+        <div>
+          <img
+            class="profile-img"
+            src="../assets/gopher/gif/gopher-computer.gif"
+            alt=""
+          />
+          <h2>Hello.</h2>
+
+          <div class="row g-0 d-flex align-items-center justify-content-center">
+            <div class="col-md-6 col-sm-6 justify-content-end">
+              <p>
+                Hi, I'm yongteng. Lu Xun once said, "Yongteng is a good boy",
+                and Yu Wei said, "It's hard to say". Ha! Who knows, I'd say,
+                "Although he doesn't seem easy to approach, he's still a decent
+                and decent person". "虽然他看上去不是很好接近,
+                但其实他的为人还是很正直讲义气~"
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr />
+
         <h2>My Skills.</h2>
-        <div class="skill-row">
-          <img class="code-img" src="../assets/me/imgs/200w.webp" alt="" />
-          <h3>Lorem & Ipsum</h3>
-          <p>
-            Lorem ipsum dolor sit amet, quis in duis, iaculis id felis.
-            Consectetuer vestibulum, nunc urna lectus, erat ligula. Hendrerit
-            nam, lectus ante, ut lorem eros.
-          </p>
+        <div class="skills">
+          <div
+            class="skill-section row g-0 d-flex align-items-center justify-content-center"
+          >
+            <div class="col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/dance.gif"
+                alt=""
+              />
+            </div>
+
+            <div class="col-md-6">
+              <h3>Golang</h3>
+              <p>
+                Three years of Golang development experience, and as you can
+                see, I love this guy.
+              </p>
+            </div>
+          </div>
+
+          <!-- Rust -->
+          <div
+            class="skill-section row g-0 d-flex align-items-center justify-content-center"
+          >
+            <div class="col-md-6">
+              <h3>Rust</h3>
+              <p>
+                New to Rust, looking forward to becoming a big guy. Just like...
+              </p>
+            </div>
+
+            <div class="col-md-2 justify-content-start">
+              <img
+                class="img-fluid rounded-circle"
+                src="../assets/gopher/gif/rustdance.gif"
+                alt=""
+              />
+            </div>
+
+            <div
+              class="row g-0 d-flex align-items-center justify-content-center"
+            >
+              <div class="col-md-1">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="../assets/gopher/gif/rust-gopher.gif"
+                  alt=""
+                />
+              </div>
+              <div class="col-md-1">Hi</div>
+            </div>
+          </div>
+
+          <!-- Linux -->
+          <div
+            class="skill-section row g-0 d-flex align-items-center justify-content-center"
+          >
+            <div class="col-sm-2 col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/swan_hack-dab.gif"
+                alt=""
+              />
+            </div>
+
+            <div class="col-md-6 col-sm-6">
+              <h3>Linux</h3>
+              <p>
+                Oh, sorry Linux, so late to introduce you. Yes, using Linux has
+                made me special, and made me a lot of good friends. We all love
+                LEC! LEC! Hooray!!!
+              </p>
+            </div>
+          </div>
+
+          <!-- Vim -->
+          <div
+            class="skill-section row g-0 d-flex align-items-center justify-content-center"
+          >
+            <div class="col-sm-2 col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/linux-ethandud.gif"
+                alt=""
+              />
+            </div>
+            <div class="col-md-6 col-sm-6">
+              <div class="d-flex align-items-center justify-content-center">
+                <h3>
+                  Vim
+                  <img
+                    class=""
+                    src="../assets/gopher/gif/vim-linux.gif"
+                    alt=""
+                  />
+                </h3>
+              </div>
+              <p>
+                There is no reason for FYP if it is not for the
+                "acknowledgements". If not for Vim, I would probably have given
+                up on Linux. Vim! Hooray!!!
+              </p>
+            </div>
+
+            <div class="col-sm-2 col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/linux-ethandud.gif"
+                alt=""
+              />
+            </div>
+
+            <div
+              class="row g-0 d-flex align-items-center justify-content-center"
+            >
+              <div class="col-sm-2 col-md-2 justify-content-end">
+                <img
+                  class="img-fluid rounded"
+                  src="../assets/gopher/gif/vim-go.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="skill-row">
-          <img class="code-img2" src="../assets/me/imgs/200w.webp" alt="" />
-          <h3>Lorem Ipsum Dolor</h3>
-          <p>
-            Lorem ipsum dolor sit amet, mauris sed consectetuer. Etiam et eu,
-            bibendum interdum, lacus quis mauris. Curabitur wisi, quisque vel
-            eu, rutrum nam.
-          </p>
-        </div>
-      </div>
-      <hr />
-      <div class="contact-me">
-        <h2>Get In Touch</h2>
-        <h3>Lorem ipsum dolor sit amet, non elit.</h3>
-        <p class="contact-message">
-          Lorem ipsum dolor sit amet, in quis, aenean amet. Phasellus sodales,
-          tellus donec dui, ornare erat.
-        </p>
-        <a class="btn" href="mailto:3065588496@qq.com">CONTACT ME</a>
+        <hr />
+
+        <section class="dream">
+          <div class="row g-0 d-flex align-items-center justify-content-center">
+            <div class="col-sm-2 col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/dance.gif"
+                alt=""
+              />
+            </div>
+            <div class="col-sm-2 col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/party-gopher.gif"
+                alt=""
+              />
+            </div>
+            <div class="col-sm-2 col-md-2 justify-content-end">
+              <img
+                class="img-fluid rounded"
+                src="../assets/gopher/gif/gopher-dance-long-3x-sign.gif"
+                alt=""
+              />
+            </div>
+          </div>
+          <p class="stop">Time is limited, so let's stop here.</p>
+        </section>
       </div>
     </div>
-    <!--
-    <div class="bottom-container">
-      <a class="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
-      <a class="footer-link" href="https://twitter.com/">Twitter</a>
-      <a class="footer-link" href="https://www.appbrewery.co/">Website</a>
-      <p class="copyright">© Yongteng Lei</p>
-    </div>
-    -->
   </div>
 </template>
 
@@ -75,14 +206,19 @@ export default {
   font-family: 'Merriweather', sans-serif;
 }
 
+.profile-img {
+  width: 20%;
+  border-radius: 50%;
+}
+
 .top {
   background-color: #b1afff;
   position: relative;
   padding-top: 100px;
 }
 
-.middle-container {
-  margin: 100px 0;
+.container-fluid {
+  padding: 3% 15%;
 }
 
 h1 {
@@ -95,13 +231,15 @@ h1 {
 
 h2 {
   font-family: 'Montserrat', sans-serif;
-  font-size: 2.5rem;
+  font-size: 3.5rem;
   color: #3f3b6c;
   font-weight: normal;
   margin-bottom: 10px;
 }
 
 h3 {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 2.5rem;
   color: #98a8f8;
 }
 
@@ -129,6 +267,10 @@ hr {
   margin: auto;
 }
 
+.skill-section {
+  margin-top: 64px;
+}
+
 .skill-row {
   width: 50%;
   margin: 100px auto;
@@ -138,18 +280,8 @@ hr {
   text-align: left;
 }
 
-.code-img {
-  width: 20%;
-  border-radius: 50%;
-  float: left;
-  margin-right: 30px;
-}
-
-.code-img2 {
-  width: 20%;
-  border-radius: 50%;
-  float: right;
-  margin-left: 30px;
+.rust-gopher-img {
+  width: 30%;
 }
 
 .contact-message {
@@ -211,6 +343,9 @@ hr {
 
 .bottom-container a:hover {
   color: #eaf6f6;
+}
+.stop {
+  margin-top: 5%;
 }
 
 .copyright {
