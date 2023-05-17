d7c73535f0b72dbb3d3ce8b7680b98c1e2a9eec5
rey <3065588496@qq.com>
2023-05-18 03:21:38 +0800

    LearnMore/divide_and_choose

--- src/views/LearnMore/DC.vue
+++ src/views/LearnMore/DC.vue
@@ -12,7 +12,7 @@
         </div>
 
         <div class="col-lg-4">
-          <h1>COMING SOON! LOL 🍻</h1>
+          <h1>Divide and Choose</h1>
           <div class="mt-3">
             <button
               type="button"
@@ -47,6 +47,165 @@
         </div>
       </div>
       <marquee>Learn more about Divide and Choose</marquee>
+    </div>
+  </section>
+
+  <section class="content">
+    <div class="container-fluid">
+      <h1>Divide and Choose</h1>
+      <div
+        class="explanation row d-flex align-items-center align-content-center justify-content-center"
+      >
+        <div class="col-lg-8 col-md-8">
+          <p>
+            If there are two agents here, Alice and Bob, and 5 items are
+            evaluated and need to be allocated.
+          </p>
+
+          <div class="mt-3">
+            <table class="table table-bordered" style="text-align: center">
+              <thead>
+                <tr>
+                  <th colspan="6" scope="col">Divide and Choose (DC)</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <th scope="row"></th>
+                  <td>Item1</td>
+                  <td>Item2</td>
+                  <td>Item3</td>
+                  <td>Item4</td>
+                  <td>Item5</td>
+                </tr>
+                <tr>
+                  <th scope="row">Alice</th>
+                  <td>12</td>
+                  <td>37</td>
+                  <td>16</td>
+                  <td>22</td>
+                  <td>13</td>
+                </tr>
+                <tr>
+                  <th scope="row">Bob</th>
+                  <td>2</td>
+                  <td>17</td>
+                  <td>27</td>
+                  <td>16</td>
+                  <td>38</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <p>Then, here is the allocation as given by DC.</p>
+
+          <div class="mt-3">
+            <table class="table table-bordered" style="text-align: center">
+              <thead>
+                <tr>
+                  <th colspan="7" scope="col" style="text-align: center">
+                    Divide and Choose (DC)
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <th scope="row"></th>
+                  <td>Item1</td>
+                  <td>Item2</td>
+                  <td>Item3</td>
+                  <td>Item4</td>
+                  <td>Item5</td>
+                  <td>Utility</td>
+                </tr>
+                <tr>
+                  <th scope="row">Alice</th>
+                  <td class="table-success">12</td>
+                  <td>37</td>
+                  <td class="table-success">16</td>
+                  <td class="table-success">22</td>
+                  <td>13</td>
+                  <td>50</td>
+                </tr>
+                <tr>
+                  <th scope="row">Bob</th>
+                  <td>2</td>
+                  <td class="table-info">17</td>
+                  <td>27</td>
+                  <td>16</td>
+                  <td class="table-info">38</td>
+                  <td>55</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <p>Why? This is the best grouping Divider has found.</p>
+          <p>
+            (If Divider wants to maximize his/her utility, the only way to do so
+            is to divide the items into two bundles as much as possible. )
+          </p>
+
+          <div class="mt-3">
+            <table class="table table-bordered" style="text-align: center">
+              <thead>
+                <tr>
+                  <th colspan="7" scope="col" style="text-align: center">
+                    Best Grouping by Divider (Alice's perspective)
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr class="align-middle">
+                  <th scope="row">Group</th>
+                  <td>Item1</td>
+                  <td>Item2</td>
+                  <td>Item3</td>
+                  <td>Item4</td>
+                  <td>Item5</td>
+                  <td>Utility Gained</td>
+                </tr>
+                <tr>
+                  <th scope="row">Group1</th>
+                  <td>12</td>
+                  <td>0</td>
+                  <td>16</td>
+                  <td>22</td>
+                  <td>0</td>
+                  <td>50</td>
+                </tr>
+                <tr>
+                  <th scope="row">Group2</th>
+                  <td>0</td>
+                  <td>37</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>13</td>
+                  <td>50</td>
+                </tr>
+                <tr>
+                  <th scope="row">Difference</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>0</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <p>
+            Bob will take one of his favourite bundle and leave the other to
+            Alice.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -91,27 +250,13 @@ h1 {
   padding: 3% 15%;
 }
 
+.explanation {
+  text-align: left;
+}
+
 .title-image {
   width: 100%;
   /*transform: rotate(25deg);*/
-}
-
-#cta {
-  background-color: #ff4c68;
-  color: #fff;
-  padding: 7% 15%;
-  text-align: center;
-}
-
-.cta-heading {
-  font-size: 3.5rem;
-  font-family: 'Montserrat';
-  line-height: 1.5;
-  font-weight: 900;
-}
-
-.cta-btn {
-  margin: 5% 0 0 0;
 }
 
 marquee {
